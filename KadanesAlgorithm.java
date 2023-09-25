ac65dc44dd26fc282918a8e12f34867ac64c85c3
Ragulraj <124597431+Ragulraj005@users.noreply.github.com>
2023-09-25 14:51:52 +0530

    Add files via upload

---
+++ KadanesAlgorithm.java
@@ -0,0 +1,19 @@
+public class KadanesAlgorithm {
+    public static int maxSubArraySum(int[] nums) {
+        long maxEndingHere = nums[0];
+        long maxSoFar = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum = maxSubArraySum(nums);
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+    }
+}
